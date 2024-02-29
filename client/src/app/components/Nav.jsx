82e9062d6150ca4eb68b8a868c5377fdfe83f64f
Sidhant Mishra <127239653+SidhantCodes@users.noreply.github.com>
2024-03-01 01:16:43 +0530

    Update Nav.jsx

--- client/src/app/components/Nav.jsx
+++ client/src/app/components/Nav.jsx
@@ -3,29 +3,12 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 const Nav = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
-
-  const handleLogin = () => {
-    //
-    setIsLoggedIn(true)
-  }
-  const handleLogout = () => {
-    setIsLoggedIn(false)
-  }
-  const onSignOut = () => {
-    console.log("Signin out")
-  }
   return (
     <nav className='flex justify-between items-center'>
       <div className="logo font-oswald-title mx-5 mt-4">
         <span className="text-[#4F96FB]">Net</span>
         <span className="text-black">Guardian</span>
       </div>
-      {isLoggedIn ? (
-        <Link href="/" className='mr-5 border-black border-solid border-[2px] flex px-4 py-1 mt-4 rounded-full hover:bg-black hover:text-white signin'>
-          <button onClick={onSignOut} className='font-outfit font-bold'>Sign Out</button>
-        </Link>
-      ) : null}
     </nav>
   );
 };
