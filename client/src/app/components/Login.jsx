057ed263c0dd9b1b164761a19a01cb126285cfc0
Sidhant Mishra <127239653+SidhantCodes@users.noreply.github.com>
2024-03-01 01:16:10 +0530

    Update Login.jsx

--- client/src/app/components/Login.jsx
+++ client/src/app/components/Login.jsx
@@ -1,27 +1,23 @@
 import Image from "next/image"
-import { bg } from "../../../public"
 import Link from "next/link"
+import { first } from "../../../public"
 const Login = () => {
   return (
-    <section className="flex items-center justify-center ">
-      <div className="form flex flex-col">
-        <p className="font-outfit text-[25px] mb-5">Login to your account</p>
-        <form method="post" className="flex flex-col">
-          <input type="text" name="userid" id="userid" placeholder="Admin Email" className="bg-white"/>
-          <input type="text" name="pwd" id="pwd" placeholder="Admin Password" />
-          <div className="btn flex justify-between mt-6">
-          <button className="text-gray-600 hover:text-black">Forgot your password?</button>
-          <Link href='/upload'>
-            <button className="signin bg-[#4F96FB] rounded-full text-white px-8 py-1 hover:bg-[#73adff]">Sign in</button>
-          </Link>
-          </div>
-        </form>
+    <div className="flex mt-10 justify-around">
+
+      <div className=" h-[80vh] ml-12 w-[45vw] mt-24" >
+        <h1 className="text-5xl font-bold">Safeguarding Your Network with Advanced Configuration Analysis</h1>
+        <p className="mt-6 text-2xl font-normal ">NetGuardian: Your comprehensive solution for advanced network configuration security analysis.</p>
+        <Link href={`/upload`}>
+        <button className="animation bg-[#4F96FB] text-white w-[200px] mt-6 h-[50px] rounded-full hover:bg-[#f1f1f1] hover:text-[#4F96FB] hover:border-4 hover:border-solid hover:border-[#4F96FB] font-semibold">Get Started</button>
+        </Link>
       </div>
 
-      <div className="img">
-        <Image src={bg} width={744} height={760}  />
+      <div className="m-10 mr-20">
+        <Image src={first} height={740} width={540} />
       </div>
-    </section>
+    </div>
+
   )
 }
 
