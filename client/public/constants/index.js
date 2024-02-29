0e0e738d8be1818c683455bf37b74ba97833de26
Sidhant Mishra <127239653+SidhantCodes@users.noreply.github.com>
2024-03-01 01:22:08 +0530

    Update index.js

--- client/public/constants/index.js
+++ client/public/constants/index.js
@@ -1,48 +1,14 @@
 export const severe = [
   {
-    color: '#62df00',
-    desc: "No severity 1 or severity 2 findings in device configuration file"
+    color: 'bg-green-500',
+    desc: "Severity 1 findings in device configuration file"
   },
   {
-    color: '#ffff00',
-    desc: "No severity 1 findings in device configuration file"
+    color: 'bg-yellow-300',
+    desc: "Severity 2 findings in device configuration file"
   },
   {
-    color: '#ff0000',
-    desc: "One or more severity 1 findings in device configuration file"
+    color: 'bg-red-500',
+    desc: "Severity 3 findings in device configuration file"
   },
 ];
-
-export const report = [
-  {
-    deviceName: "Device 1",
-    riskColor: '#62df00',
-    reportUrl: 'URL string'
-  },
-  {
-    deviceName: "Device 2",
-    riskColor: '#edf200',
-    reportUrl: 'URL string'
-  },
-  {
-    deviceName: "Device 3",
-    riskColor: '#edf200',
-    reportUrl: 'URL string'
-  },
-  {
-    deviceName: "Device 4",
-    riskColor: '#62df00',
-    reportUrl: 'URL string'
-  },
-  {
-    deviceName: "Device 2",
-    riskColor: '#62df00',
-    reportUrl: 'URL string'
-  },
-  {
-    deviceName: "Device 1",
-    riskColor: '#ff0000',
-    reportUrl: 'URL string'
-  },
-
-]
